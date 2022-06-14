8eaa839475e81147c1b95c05a22b1ddc1a88fcfd
rickydomingos <ricardodomingos97@gmail.com>
2022-06-14 13:17:32 -0300

    dia 3 fixing bugs resposta desaparece e nao podemos mais perguntar

---
+++ .history/script_20220612134302.js
@@ -0,0 +1,24 @@
+const respostas = [
+    "Certeza!",
+    "Não tenho tanta certeza.",
+    "É decididamente assim.",
+    "Não conte com isso.",
+    "Sem dúvidas!",
+    "Pergunte novamente mais tarde.",
+    "Sim, definitivamente!",
+    "Minha resposta é não.",
+    "Você pode contar com isso.",
+    "Melhor não te dizer agora.",
+    "A meu ver, sim.",
+    "Minhas fontes dizem não.",
+    "Provavelmente.",
+    "Não é possível prever agora.",
+    "Perspectiva boa.",
+    "As perspectivas não são tão boas.",
+    "Sim.",
+    "Concentre-se e pergunte novamente.",
+    "Sinais apontam que sim.",
+  ]
+
+  // gerar numero aleatorio
+  const respostas = 
---
+++ .history/script_20220614131630.js
@@ -0,0 +1,49 @@
+const respostaElement = document.querySelector("#resposta")
+
+const inputPergunta = document.querySelector("#inputPergunta")
+const respostas = [
+  "Certeza!",
+  "Não tenho tanta certeza.",
+  "É decididamente assim.",
+  "Não conte com isso.",
+  "Sem dúvidas!",
+  "Pergunte novamente mais tarde.",
+  "Sim, definitivamente!",
+  "Minha resposta é não.",
+  "Você pode contar com isso.",
+  "Melhor não te dizer agora.",
+  "A meu ver, sim.",
+  "Minhas fontes dizem não.",
+  "Provavelmente.",
+  "Não é possível prever agora.",
+  "Perspectiva boa.",
+  "As perspectivas não são tão boas.",
+  "Sim.",
+  "Concentre-se e pergunte novamente.",
+  "Sinais apontam que sim.",
+]
+
+// clicar em fazer pergunta
+function fazerPergunta() {
+
+  if(inputPergunta.value == "") {
+    alert("Digite a sua pergunta!")
+    return
+  }
+
+  const pergunta = "<div>" + inputPergunta.value + "</div>"
+
+
+  // gerar numero aleatorio
+  const totalRespostas = respostas.length
+  const numeroAleatorio = Math.floor(Math.random() * totalRespostas)
+
+  respostaElement.innerHTML = pergunta + respostas[numeroAleatorio]
+  espostaElement.style.opacity = 1;// depois desaparecer da tela podemos 
+
+  // sumir a resposta depois de 3 segundos
+  setTimeout(() => {
+    respostaElement.style.opacity = 0;
+  }, 3000)
+
+}
